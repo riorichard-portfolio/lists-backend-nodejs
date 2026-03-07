f316dc68ab623672318da03e2494e8e5dcb788dc
riorichard98 <riorichard12@gmail.com>
2026-03-07 13:50:40 +0700

    feat: sql db abstraction interface

---
+++ src/.domains/.shared.domain/sql.db.ts
@@ -0,0 +1,28 @@
+export type TParam = string | number | boolean | null
+export interface IQuerySchema {
+    [key: string]: 'string' | 'number' | 'boolean'
+}
+
+export type TSchemaToType<T extends IQuerySchema> = {
+    [K in keyof T]:
+    T[K] extends 'string' ? string :
+    T[K] extends 'number' ? number :
+    T[K] extends 'boolean' ? boolean :
+    never
+}
+
+export interface ISqlQuery {
+    createSqlParams(): TParam[]
+    query(sql: string, params: TParam[]): Promise<null>
+    query<const QuerySchema extends IQuerySchema>(
+        sql: string,
+        params: TParam[], // force to give any params , even limit or offset
+        schema?: QuerySchema
+    ): Promise<TSchemaToType<QuerySchema>[]>
+}
+
+export interface ITransactionQueries extends ISqlQuery { }
+
+export interface ISqlTransaction {
+    sqlTransaction(executeQueries: (transactionQueries: ITransactionQueries) => Promise<void>): Promise<void>
+}
