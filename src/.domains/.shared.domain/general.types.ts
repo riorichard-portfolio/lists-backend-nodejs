48932c9b8be865d0b9f63f08baf539a062e74fcf
riorichard98 <riorichard12@gmail.com>
2026-03-27 19:50:23 +0700

    feat: general types for boilerplate

---
+++ src/.domains/.shared.domain/general.types.ts
@@ -0,0 +1,20 @@
+export type ISuccessResult<ResultData> = { success: true } & ResultData
+
+export interface IFailedResult<FailedReason extends string> {
+    success: false
+    failedReason(): FailedReason
+}
+
+export type TApplicationResults<ResultData, FailedReason extends string> =
+    | ISuccessResult<ResultData>
+    | IFailedResult<FailedReason>
+
+export type IFoundRepositoryData<RepositoryData> = { found: true } & RepositoryData
+
+export interface INotFoundRepositoryData {
+    found: false
+}
+
+export type TRepositoryResults<RepositoryData> =
+    | IFoundRepositoryData<RepositoryData>
+    | INotFoundRepositoryData
