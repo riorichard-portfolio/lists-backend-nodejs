0d5779e9e59d1c1832a98d5b7eba5ed459ee9ace
Rio Richard <riorichard@pnm.co.id>
2026-04-24 16:42:04 +0700

    feat: status & set done in todo entity

--- src/.domains/todo.domain/todo.entities.ts
+++ src/.domains/todo.domain/todo.entities.ts
@@ -2,8 +2,10 @@ export interface ITodoEntity {
     getTodoId(): string
     getDescription(): string
     getCreatedAt(): number
+    getStatus(): 'DONE' | 'NOT_DONE'
+    setDone(): void
 }
 
 export interface ITodoEntitiesFactory {
-    createTodoEntity(todoId: string, description: string, createdAt: number): ITodoEntity
+    createTodoEntity(todoId: string, description: string, createdAt: number, status: 'DONE' | 'NOT_DONE'): ITodoEntity
 }
