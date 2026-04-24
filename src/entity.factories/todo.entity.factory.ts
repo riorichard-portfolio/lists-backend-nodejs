deeb3877c07b3f205bce86a705b1b15d01c337e6
Rio Richard <riorichard@pnm.co.id>
2026-04-24 16:42:39 +0700

    feat: todo entity impelementation

--- src/entity.factories/todo.entity.factory.ts
+++ src/entity.factories/todo.entity.factory.ts
@@ -7,7 +7,8 @@ class TodoEntity implements ITodoEntity {
     constructor(
         private readonly todoId: string,
         private readonly description: string,
-        private readonly createdAt: number
+        private readonly createdAt: number,
+        private status: 'DONE' | 'NOT_DONE'
     ) { }
 
     public getTodoId(): string {
@@ -21,10 +22,18 @@ class TodoEntity implements ITodoEntity {
     public getCreatedAt(): number {
         return this.createdAt
     }
+
+    public getStatus(): "DONE" | "NOT_DONE" {
+        return this.status
+    }
+
+    public setDone(): void {
+        this.status = 'DONE'
+    }
 }
 
 export class TodoEntitiesFactory implements ITodoEntitiesFactory {
-    public createTodoEntity(todoId: string, description: string, createdAt: number): ITodoEntity {
-        return new TodoEntity(todoId, description, createdAt)
+    public createTodoEntity(todoId: string, description: string, createdAt: number, status: 'DONE' | 'NOT_DONE'): ITodoEntity {
+        return new TodoEntity(todoId, description, createdAt, status)
     }
 }
